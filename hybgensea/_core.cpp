f499af74c12132100f4b3940d8e2cdd689bc64a9
Mauricio Souza de Alencar <856825+mdealencar@users.noreply.github.com>
2026-03-02 15:53:25 +0100

    Use official HGS C interface declarations in nanobind module

---
+++ hybgensea/_core.cpp
@@ -0,0 +1,173 @@
+#include <nanobind/nanobind.h>
+#include <nanobind/ndarray.h>
+#include <nanobind/stl/string.h>
+#include <nanobind/stl/vector.h>
+
+#include "C_Interface.h"
+
+#include <cstdio>
+#include <stdexcept>
+#include <unistd.h>
+
+namespace nb = nanobind;
+using namespace nb::literals;
+
+class ScopedStdoutFdRedirect {
+public:
+    ScopedStdoutFdRedirect() {
+        nb::object sys = nb::module_::import_("sys");
+        nb::object stdout_obj = sys.attr("stdout");
+        int py_stdout_fd = nb::cast<int>(stdout_obj.attr("fileno")());
+
+        std::fflush(stdout);
+        saved_stdout_fd_ = dup(STDOUT_FILENO);
+        if (saved_stdout_fd_ == -1) {
+            throw std::runtime_error("Failed to duplicate STDOUT_FILENO.");
+        }
+
+        if (dup2(py_stdout_fd, STDOUT_FILENO) == -1) {
+            close(saved_stdout_fd_);
+            throw std::runtime_error("Failed to redirect STDOUT_FILENO to sys.stdout.");
+        }
+    }
+
+    ~ScopedStdoutFdRedirect() {
+        if (saved_stdout_fd_ != -1) {
+            std::fflush(stdout);
+            dup2(saved_stdout_fd_, STDOUT_FILENO);
+            close(saved_stdout_fd_);
+        }
+    }
+
+    ScopedStdoutFdRedirect(const ScopedStdoutFdRedirect &) = delete;
+    ScopedStdoutFdRedirect &operator=(const ScopedStdoutFdRedirect &) = delete;
+
+private:
+    int saved_stdout_fd_ = -1;
+};
+
+static nb::dict to_python_solution(Solution *sol) {
+    if (!sol) {
+        throw std::runtime_error("HGS-CVRP returned a null solution pointer.");
+    }
+
+    nb::list routes;
+    for (int i = 0; i < sol->n_routes; i++) {
+        nb::list path;
+        for (int j = 0; j < sol->routes[i].length; j++) {
+            path.append(sol->routes[i].path[j]);
+        }
+        routes.append(path);
+    }
+
+    nb::dict result;
+    result["cost"] = sol->cost;
+    result["time"] = sol->time;
+    result["n_routes"] = sol->n_routes;
+    result["routes"] = routes;
+    return result;
+}
+
+NB_MODULE(_core, m) {
+    m.def(
+        "solve_cvrp",
+        [](int n,
+           nb::ndarray<double, nb::c_contig, nb::device::cpu> x,
+           nb::ndarray<double, nb::c_contig, nb::device::cpu> y,
+           nb::ndarray<double, nb::c_contig, nb::device::cpu> service_times,
+           nb::ndarray<double, nb::c_contig, nb::device::cpu> demand,
+           double vehicle_capacity, double duration_limit, bool is_rounding_integer,
+           bool is_duration_constraint, int max_nb_veh,
+           int nb_granular, int mu, int lambda_value, int nb_elite, int nb_close,
+           int nb_iter_penalty_management, double target_feasible,
+           double penalty_decrease, double penalty_increase, int seed, int nb_iter,
+           int nb_iter_traces, double time_limit, bool use_swap_star, bool verbose) {
+            AlgorithmParameters ap = {
+                nb_granular,
+                mu,
+                lambda_value,
+                nb_elite,
+                nb_close,
+                nb_iter_penalty_management,
+                target_feasible,
+                penalty_decrease,
+                penalty_increase,
+                seed,
+                nb_iter,
+                nb_iter_traces,
+                time_limit,
+                static_cast<int>(use_swap_star),
+            };
+
+            ScopedStdoutFdRedirect redirect;
+            Solution *sol = solve_cvrp(
+                n, x.data(), y.data(), service_times.data(), demand.data(),
+                vehicle_capacity, duration_limit,
+                static_cast<char>(is_rounding_integer),
+                static_cast<char>(is_duration_constraint), max_nb_veh, &ap,
+                static_cast<char>(verbose));
+
+            nb::dict result = to_python_solution(sol);
+            delete_solution(sol);
+            return result;
+        },
+        "n"_a, "x"_a, "y"_a, "service_times"_a, "demand"_a,
+        "vehicle_capacity"_a, "duration_limit"_a, "is_rounding_integer"_a,
+        "is_duration_constraint"_a, "max_nb_veh"_a,
+        "nb_granular"_a, "mu"_a, "lambda_value"_a, "nb_elite"_a, "nb_close"_a,
+        "nb_iter_penalty_management"_a, "target_feasible"_a,
+        "penalty_decrease"_a, "penalty_increase"_a, "seed"_a, "nb_iter"_a,
+        "nb_iter_traces"_a, "time_limit"_a, "use_swap_star"_a, "verbose"_a
+    );
+
+    m.def(
+        "solve_cvrp_dist_mtx",
+        [](int n,
+           nb::ndarray<double, nb::c_contig, nb::device::cpu> x,
+           nb::ndarray<double, nb::c_contig, nb::device::cpu> y,
+           nb::ndarray<double, nb::c_contig, nb::device::cpu> dist_mtx,
+           nb::ndarray<double, nb::c_contig, nb::device::cpu> service_times,
+           nb::ndarray<double, nb::c_contig, nb::device::cpu> demand,
+           double vehicle_capacity, double duration_limit,
+           bool is_duration_constraint, int max_nb_veh,
+           int nb_granular, int mu, int lambda_value, int nb_elite, int nb_close,
+           int nb_iter_penalty_management, double target_feasible,
+           double penalty_decrease, double penalty_increase, int seed, int nb_iter,
+           int nb_iter_traces, double time_limit, bool use_swap_star, bool verbose) {
+            AlgorithmParameters ap = {
+                nb_granular,
+                mu,
+                lambda_value,
+                nb_elite,
+                nb_close,
+                nb_iter_penalty_management,
+                target_feasible,
+                penalty_decrease,
+                penalty_increase,
+                seed,
+                nb_iter,
+                nb_iter_traces,
+                time_limit,
+                static_cast<int>(use_swap_star),
+            };
+
+            ScopedStdoutFdRedirect redirect;
+            Solution *sol = solve_cvrp_dist_mtx(
+                n, x.data(), y.data(), dist_mtx.data(), service_times.data(), demand.data(),
+                vehicle_capacity, duration_limit,
+                static_cast<char>(is_duration_constraint), max_nb_veh, &ap,
+                static_cast<char>(verbose));
+
+            nb::dict result = to_python_solution(sol);
+            delete_solution(sol);
+            return result;
+        },
+        "n"_a, "x"_a, "y"_a, "dist_mtx"_a, "service_times"_a, "demand"_a,
+        "vehicle_capacity"_a, "duration_limit"_a, "is_duration_constraint"_a,
+        "max_nb_veh"_a,
+        "nb_granular"_a, "mu"_a, "lambda_value"_a, "nb_elite"_a, "nb_close"_a,
+        "nb_iter_penalty_management"_a, "target_feasible"_a,
+        "penalty_decrease"_a, "penalty_increase"_a, "seed"_a, "nb_iter"_a,
+        "nb_iter_traces"_a, "time_limit"_a, "use_swap_star"_a, "verbose"_a
+    );
+}
--- hybgensea/hybgensea.py
+++ hybgensea/hybgensea.py
@@ -1,62 +1,11 @@
-import os
-import platform
-from ctypes import (
-    Structure,
-    CDLL,
-    POINTER,
-    c_int,
-    c_double,
-    c_char,
-    sizeof,
-    cast,
-    byref,
-)
 from dataclasses import dataclass
 import numpy as np
 import sys
 
+from . import _core
 
-def get_lib_filename():
-    if platform.system() == "Linux":
-        lib_ext = "so"
-    elif platform.system() == "Darwin":
-        lib_ext = "dylib"
-    elif platform.system() == "Windows":
-        lib_ext = "dll"
-    else:
-        lib_ext = "so"
-    return f"libhgscvrp.{lib_ext}"
-
-
-# basedir = os.path.abspath(os.path.dirname(__file__))
-basedir = os.path.dirname(os.path.realpath(__file__))
-# os.add_dll_directory(basedir)
-HGS_LIBRARY_FILEPATH = os.path.join(basedir, get_lib_filename())
-
-c_double_p = POINTER(c_double)
-c_int_p = POINTER(c_int)
-C_INT_MAX = 2 ** (sizeof(c_int) * 8 - 1) - 1
+C_INT_MAX = 2 ** 31 - 1
 C_DBL_MAX = sys.float_info.max
-
-
-# Must match with AlgorithmParameters.h in HGS-CVRP: https://github.com/vidalt/HGS-CVRP
-class CAlgorithmParameters(Structure):
-    _fields_ = [
-        ("nbGranular", c_int),
-        ("mu", c_int),
-        ("lambda", c_int),
-        ("nbElite", c_int),
-        ("nbClose", c_int),
-        ("nbIterPenaltyManagement", c_int),
-        ("targetFeasible", c_double),
-        ("penaltyDecrease", c_double),
-        ("penaltyIncrease", c_double),
-        ("seed", c_int),
-        ("nbIter", c_int),
-        ("nbIterTraces", c_int),
-        ("timeLimit", c_double),
-        ("useSwapStar", c_int),
-    ]
 
 
 @dataclass
@@ -76,127 +25,37 @@ class AlgorithmParameters:
     timeLimit: float = 0.0
     useSwapStar: bool = True
 
-    @property
-    def ctypes(self) -> CAlgorithmParameters:
-        return CAlgorithmParameters(
-            self.nbGranular,
-            self.mu,
-            self.lambda_,
-            self.nbElite,
-            self.nbClose,
-            self.nbIterPenaltyManagement,
-            self.targetFeasible,
-            self.penaltyDecrease,
-            self.penaltyIncrease,
-            self.seed,
-            self.nbIter,
-            self.nbIterTraces,
-            self.timeLimit,
-            int(self.useSwapStar),
-        )
-
-
-class _SolutionRoute(Structure):
-    _fields_ = [("length", c_int), ("path", c_int_p)]
-
-
-class _Solution(Structure):
-    _fields_ = [
-        ("cost", c_double),
-        ("time", c_double),
-        ("n_routes", c_int),
-        ("routes", POINTER(_SolutionRoute)),
-    ]
-
 
 class RoutingSolution:
-    def __init__(self, sol_ptr):
-        if not sol_ptr:
-            raise TypeError("The solution pointer is null.")
-
-        self.cost = sol_ptr[0].cost
-        self.time = sol_ptr[0].time
-        self.n_routes = sol_ptr[0].n_routes
-        self.routes = []
-        for i in range(self.n_routes):
-            r = sol_ptr[0].routes[i]
-            path = r.path[0 : r.length]
-            self.routes.append(path)
+    def __init__(self, payload: dict):
+        self.cost = payload["cost"]
+        self.time = payload["time"]
+        self.n_routes = payload["n_routes"]
+        self.routes = [list(route) for route in payload["routes"]]
 
 
 class Solver:
     def __init__(self, parameters=AlgorithmParameters(), verbose=True):
-        if platform.system() == "Windows":
-            hgs_library = CDLL(HGS_LIBRARY_FILEPATH, winmode=0)
-        else:
-            hgs_library = CDLL(HGS_LIBRARY_FILEPATH)
-
         self.algorithm_parameters = parameters
         self.verbose = verbose
 
-        # solve_cvrp
-        self._c_api_solve_cvrp = hgs_library.solve_cvrp
-        self._c_api_solve_cvrp.argtypes = [
-            c_int,
-            c_double_p,
-            c_double_p,
-            c_double_p,
-            c_double_p,
-            c_double,
-            c_double,
-            c_char,
-            c_char,
-            c_int,
-            POINTER(CAlgorithmParameters),
-            c_char,
-        ]
-        self._c_api_solve_cvrp.restype = POINTER(_Solution)
-
-        # solve_cvrp_dist_mtx
-        self._c_api_solve_cvrp_dist_mtx = hgs_library.solve_cvrp_dist_mtx
-        self._c_api_solve_cvrp_dist_mtx.argtypes = [
-            c_int,
-            c_double_p,
-            c_double_p,
-            c_double_p,
-            c_double_p,
-            c_double_p,
-            c_double,
-            c_double,
-            c_char,
-            c_int,
-            POINTER(CAlgorithmParameters),
-            c_char,
-        ]
-        self._c_api_solve_cvrp_dist_mtx.restype = POINTER(_Solution)
-
-        # delete_solution
-        self._c_api_delete_sol = hgs_library.delete_solution
-        self._c_api_delete_sol.restype = None
-        self._c_api_delete_sol.argtypes = [POINTER(_Solution)]
-
     def solve_cvrp(self, data, rounding=True):
-        # required data
-        demand = np.asarray(data["demands"])
+        demand = np.asarray(data["demands"], dtype=np.float64)
         vehicle_capacity = data["vehicle_capacity"]
         n_nodes = len(demand)
 
-        # optional depot
         depot = data.get("depot", 0)
         if depot != 0:
             raise ValueError("In HGS, the depot location must be 0.")
 
-        # optional num_vehicles
         maximum_number_of_vehicles = data.get("num_vehicles", C_INT_MAX)
 
-        # optional service_times
         service_times = data.get("service_times")
         if service_times is None:
-            service_times = np.zeros(n_nodes)
+            service_times = np.zeros(n_nodes, dtype=np.float64)
         else:
-            service_times = np.asarray(service_times)
+            service_times = np.asarray(service_times, dtype=np.float64)
 
-        # optional duration_limit
         duration_limit = data.get("duration_limit")
         if duration_limit is None:
             is_duration_constraint = False
@@ -212,11 +71,11 @@ class Solver:
 
         if x_coords is None or y_coords is None:
             assert dist_mtx is not None
-            x_coords = np.zeros(n_nodes)
-            y_coords = np.zeros(n_nodes)
+            x_coords = np.zeros(n_nodes, dtype=np.float64)
+            y_coords = np.zeros(n_nodes, dtype=np.float64)
         else:
-            x_coords = np.asarray(x_coords)
-            y_coords = np.asarray(y_coords)
+            x_coords = np.asarray(x_coords, dtype=np.float64)
+            y_coords = np.asarray(y_coords, dtype=np.float64)
 
         assert len(x_coords) == len(y_coords) == len(service_times) == len(demand)
         assert (x_coords >= 0.0).all()
@@ -225,7 +84,7 @@ class Solver:
         assert (demand >= 0.0).all()
 
         if dist_mtx is not None:
-            dist_mtx = np.asarray(dist_mtx)
+            dist_mtx = np.asarray(dist_mtx, dtype=np.float64)
             assert dist_mtx.shape[0] == dist_mtx.shape[1]
             assert (dist_mtx >= 0.0).all()
             return self._solve_cvrp_dist_mtx(
@@ -241,20 +100,20 @@ class Solver:
                 self.algorithm_parameters,
                 self.verbose,
             )
-        else:
-            return self._solve_cvrp(
-                x_coords,
-                y_coords,
-                service_times,
-                demand,
-                vehicle_capacity,
-                duration_limit,
-                is_rounding_integer,
-                is_duration_constraint,
-                maximum_number_of_vehicles,
-                self.algorithm_parameters,
-                self.verbose,
-            )
+
+        return self._solve_cvrp(
+            x_coords,
+            y_coords,
+            service_times,
+            demand,
+            vehicle_capacity,
+            duration_limit,
+            is_rounding_integer,
+            is_duration_constraint,
+            maximum_number_of_vehicles,
+            self.algorithm_parameters,
+            self.verbose,
+        )
 
     def solve_tsp(self, data, rounding=True):
         x_coords = data.get("x_coordinates")
@@ -272,93 +131,90 @@ class Solver:
 
         return self.solve_cvrp(data, rounding=rounding)
 
+    @staticmethod
     def _solve_cvrp(
-        self,
-        x_coords: np.ndarray,
-        y_coords: np.ndarray,
-        service_times: np.ndarray,
-        demand: np.ndarray,
-        vehicle_capacity: int,
-        duration_limit: float,
-        is_rounding_integer: bool,
-        is_duration_constraint: bool,
-        maximum_number_of_vehicles: int,
-        algorithm_parameters: AlgorithmParameters,
-        verbose: bool,
+        x_coords,
+        y_coords,
+        service_times,
+        demand,
+        vehicle_capacity,
+        duration_limit,
+        is_rounding_integer,
+        is_duration_constraint,
+        maximum_number_of_vehicles,
+        algorithm_parameters,
+        verbose,
     ):
         n_nodes = x_coords.size
-        x_ct = x_coords.astype(c_double).ctypes
-        y_ct = y_coords.astype(c_double).ctypes
-        s_ct = service_times.astype(c_double).ctypes
-        d_ct = demand.astype(c_double).ctypes
-        ap_ct = algorithm_parameters.ctypes
-
-        # struct Solution * solve_cvrp(
-        # 	int n, double* x, double* y, double* serv_time, double* dem,
-        # 	double vehicleCapacity, double durationLimit, char isRoundingInteger, char isDurationConstraint,
-        # 	int max_nbVeh, const struct AlgorithmParameters* ap, char verbose);
-        sol_p = self._c_api_solve_cvrp(
-            n_nodes,
-            cast(x_ct, c_double_p),
-            cast(y_ct, c_double_p),
-            cast(s_ct, c_double_p),
-            cast(d_ct, c_double_p),
-            vehicle_capacity,
-            duration_limit,
-            is_rounding_integer,
-            is_duration_constraint,
-            maximum_number_of_vehicles,
-            byref(ap_ct),
-            verbose,
+        payload = _core.solve_cvrp(
+            n=n_nodes,
+            x=np.ascontiguousarray(x_coords, dtype=np.float64),
+            y=np.ascontiguousarray(y_coords, dtype=np.float64),
+            service_times=np.ascontiguousarray(service_times, dtype=np.float64),
+            demand=np.ascontiguousarray(demand, dtype=np.float64),
+            vehicle_capacity=vehicle_capacity,
+            duration_limit=duration_limit,
+            is_rounding_integer=is_rounding_integer,
+            is_duration_constraint=is_duration_constraint,
+            max_nb_veh=maximum_number_of_vehicles,
+            nb_granular=algorithm_parameters.nbGranular,
+            mu=algorithm_parameters.mu,
+            lambda_value=algorithm_parameters.lambda_,
+            nb_elite=algorithm_parameters.nbElite,
+            nb_close=algorithm_parameters.nbClose,
+            nb_iter_penalty_management=algorithm_parameters.nbIterPenaltyManagement,
+            target_feasible=algorithm_parameters.targetFeasible,
+            penalty_decrease=algorithm_parameters.penaltyDecrease,
+            penalty_increase=algorithm_parameters.penaltyIncrease,
+            seed=algorithm_parameters.seed,
+            nb_iter=algorithm_parameters.nbIter,
+            nb_iter_traces=algorithm_parameters.nbIterTraces,
+            time_limit=algorithm_parameters.timeLimit,
+            use_swap_star=algorithm_parameters.useSwapStar,
+            verbose=verbose,
         )
+        return RoutingSolution(payload)
 
-        result = RoutingSolution(sol_p)
-        self._c_api_delete_sol(sol_p)
-        return result
-
+    @staticmethod
     def _solve_cvrp_dist_mtx(
-        self,
-        x_coords: np.ndarray,
-        y_coords: np.ndarray,
-        dist_mtx: np.ndarray,
-        service_times: np.ndarray,
-        demand: np.ndarray,
-        vehicle_capacity: int,
-        duration_limit: float,
-        is_duration_constraint: bool,
-        maximum_number_of_vehicles: int,
-        algorithm_parameters: AlgorithmParameters,
-        verbose: bool,
+        x_coords,
+        y_coords,
+        dist_mtx,
+        service_times,
+        demand,
+        vehicle_capacity,
+        duration_limit,
+        is_duration_constraint,
+        maximum_number_of_vehicles,
+        algorithm_parameters,
+        verbose,
     ):
         n_nodes = x_coords.size
-
-        x_ct = x_coords.astype(c_double).ctypes
-        y_ct = y_coords.astype(c_double).ctypes
-        s_ct = service_times.astype(c_double).ctypes
-        d_ct = demand.astype(c_double).ctypes
-
-        m_ct = dist_mtx.reshape(n_nodes * n_nodes).astype(c_double).ctypes
-        ap_ct = algorithm_parameters.ctypes
-
-        # struct Solution *solve_cvrp_dist_mtx(
-        # 	int n, double* x, double* y, double *dist_mtx, double *serv_time, double *dem,
-        # 	double vehicleCapacity, double durationLimit, char isDurationConstraint,
-        # 	int max_nbVeh, const struct AlgorithmParameters *ap, char verbose);
-        sol_p = self._c_api_solve_cvrp_dist_mtx(
-            n_nodes,
-            cast(x_ct, c_double_p),
-            cast(y_ct, c_double_p),
-            cast(m_ct, c_double_p),
-            cast(s_ct, c_double_p),
-            cast(d_ct, c_double_p),
-            vehicle_capacity,
-            duration_limit,
-            is_duration_constraint,
-            maximum_number_of_vehicles,
-            byref(ap_ct),
-            verbose,
+        payload = _core.solve_cvrp_dist_mtx(
+            n=n_nodes,
+            x=np.ascontiguousarray(x_coords, dtype=np.float64),
+            y=np.ascontiguousarray(y_coords, dtype=np.float64),
+            dist_mtx=np.ascontiguousarray(dist_mtx.reshape(n_nodes * n_nodes), dtype=np.float64),
+            service_times=np.ascontiguousarray(service_times, dtype=np.float64),
+            demand=np.ascontiguousarray(demand, dtype=np.float64),
+            vehicle_capacity=vehicle_capacity,
+            duration_limit=duration_limit,
+            is_duration_constraint=is_duration_constraint,
+            max_nb_veh=maximum_number_of_vehicles,
+            nb_granular=algorithm_parameters.nbGranular,
+            mu=algorithm_parameters.mu,
+            lambda_value=algorithm_parameters.lambda_,
+            nb_elite=algorithm_parameters.nbElite,
+            nb_close=algorithm_parameters.nbClose,
+            nb_iter_penalty_management=algorithm_parameters.nbIterPenaltyManagement,
+            target_feasible=algorithm_parameters.targetFeasible,
+            penalty_decrease=algorithm_parameters.penaltyDecrease,
+            penalty_increase=algorithm_parameters.penaltyIncrease,
+            seed=algorithm_parameters.seed,
+            nb_iter=algorithm_parameters.nbIter,
+            nb_iter_traces=algorithm_parameters.nbIterTraces,
+            time_limit=algorithm_parameters.timeLimit,
+            use_swap_star=algorithm_parameters.useSwapStar,
+            verbose=verbose,
         )
-
-        result = RoutingSolution(sol_p)
-        self._c_api_delete_sol(sol_p)
-        return result
+        return RoutingSolution(payload)
